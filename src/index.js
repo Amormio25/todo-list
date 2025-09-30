f087dd38fed043acd0adb2c0127b149aa0c452e4
Amormio Velasquez III <amorbuks25@gmail.com>
2025-09-30 10:06:06 -0700

    Successfully tested task branch for merge

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// TEST WORKS
+// import Task from "./modules/task";
+
+// const item = new Task("testT", "testDesc", "testDue", "testP", "testC");
+// console.log(item.getTitle());
+// item.setTitle("newTitle");
+// console.log(item.getTitle());
