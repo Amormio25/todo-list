e2a4f6ebcf4e915bb7b4f18c72c0f62f46601d3f
Amormio Velasquez III <amorbuks25@gmail.com>
2025-10-02 11:13:52 -0700

    Successfully created and tested project branch for merge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 // TEST WORKS
 // import Task from "./modules/task";
+// import Project from "./modules/project";
 
+// const proj = new Project("Project");
 // const item = new Task("testT", "testDesc", "testDue", "testP", "testC");
+// const item2 = new Task("testT", "testDesc", "testDue", "testP", "testC");
+
 // console.log(item.getTitle());
-// item.setTitle("newTitle");
-// console.log(item.getTitle());
+
+// proj.addTask(item);
+// console.log(proj.getTasks());
+// proj.addTask(item2);
+// console.log("should not be 2 tasks below")
+// console.log(proj.getTasks());
+
+// proj.removeTask(item);
+// console.log(proj.getTasks());
+// console.log(proj.getProjectTitle());
+
---
+++ src/modules/project.js
@@ -0,0 +1,35 @@
+export default class Project {
+    #title;
+    #todoList = [];
+
+    constructor(title) {
+        this.#title = title;
+    }
+
+    addTask(task) {
+        const taskExists = this.#todoList.some(item => item.title === task.title);
+        if (taskExists) {
+            console.log("Task with the same title already exists.");
+            return -1;
+        }
+        this.#todoList.push(task);
+        console.log("Task added successfully");
+        return 0;
+    }
+
+    removeTask(task) {
+        this.#todoList = this.#todoList.filter(item => item !== task);
+    }
+
+    getTasks() {
+        return [...this.#todoList];
+    }
+
+    renameProject(title) {
+        this.#title = title;
+    }
+
+    getProjectTitle() {
+        return this.#title;
+    }
+}
