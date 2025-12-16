77ba0243bb3b8446474ed9729b5f4902f280ad86
Amormio Velasquez III <amorbuks25@gmail.com>
2025-12-15 22:19:59 -0800

    fixup! Starting on work again, test commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ProjectList from "./modules/projectList";
 // div return its title and then parse the list for the title?
 // Or should it return its index and simply remove the index from projectList?
 
-//************* ProjectList test /*************/
+//************************* ProjectList test ***************************/
 // const list = new ProjectList();
 // console.log(list.getProjectList());
 
@@ -20,19 +20,26 @@ import ProjectList from "./modules/projectList";
 
 // CHECK IF ANY FUNCTIONS NEED OTHER RETURN STATEMENTS
 
-/************* Project and Task test /**************/
-const proj = new Project("Project");
-const item = new Task("testT", "testDesc", "testDue", "testP", "testC");
-const item2 = new Task("testT", "testDesc", "testDue", "testP", "testC");
+/*********************** Project and Task test ************************/
+// const proj = new Project("Project");
+// const item = new Task("testT", "testDesc", "testDue", "testP", "testC");
+// const item2 = new Task("testT", "testDesc", "testDue", "testP", "testC");
 
-console.log(item.getTitle());
+// console.log(item.getTitle());
 
-proj.addTask(item);
-console.log(proj.getTasks());
-proj.addTask(item2);
-console.log("should not be 2 tasks below");
-console.log(proj.getTasks());
+// proj.addTask(item);
+// console.log(proj.getTasks());
+// proj.addTask(item2);
+// console.log("should not be 2 tasks below");
+// console.log(proj.getTasks());
 
-proj.removeTask(item);
-console.log(proj.getTasks());
-console.log(proj.getProjectTitle());
+// proj.removeTask(item);
+// console.log(proj.getTasks());
+// console.log(proj.getProjectTitle());
+
+/******************************** Plan *********************************/
+// create basic ui with grid
+// create dialog popups for new project and new task
+//      these popups should take a form and use the ctors to create the objects
+// for editing, have the same popup but since we're not creating a new object
+//      find a way to use the getters and setters to do the work
