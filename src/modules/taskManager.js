c7238ff48cec3b44877a20f5ac20e7456caaa44b
Amormio Velasquez III <amorbuks25@gmail.com>
2026-01-07 09:37:49 -0800

    Refactoring taskManager functions to be encapsulated in one instance object

--- src/modules/taskManager.js
+++ src/modules/taskManager.js
@@ -1,44 +1,34 @@
 import { isToday, isThisWeek } from "date-fns";
 import projectList from "./projectList";
 
-// current setup
-// tasks are accessed through this flow:
-//     projectList -> getProjects -> getTasks (for each project)
-// once we get all tasks then we can have different functions to filter
+const taskManager = {
+  getAllTasks: () => {
+    const projects = projectList.getProjectList();
+    let tasks = [];
+    projects.forEach((project) => {
+      tasks = tasks.concat(project.getTasks());
+    });
+    return tasks;
+  },
+  getTodayTasks: () => {
+    const tasks = getAllTasks();
+    return tasks.filter((task) => isToday(task.getDueDate()));
+  },
 
-const getAllTasks = () => {
-  const projects = projectList.getProjectList();
-  let tasks = [];
-  projects.forEach((project) => {
-    tasks = tasks.concat(project.getTasks());
-  });
-  return tasks;
+  getUpcomingTasks: () => {
+    const tasks = getAllTasks();
+    return tasks.filter((task) => isThisWeek(task.getDueDate()));
+  },
+
+  getImportantTasks: () => {
+    const tasks = getAllTasks();
+    return tasks.filter((task) => task.getPriority() === "High");
+  },
+
+  getCompletedTasks: () => {
+    const tasks = getAllTasks();
+    return tasks.filter((task) => task.getCompletedStatus() === true);
+  },
 };
 
-const getTodayTasks = () => {
-  const tasks = getAllTasks();
-  return tasks.filter((task) => isToday(task.getDueDate()));
-};
-
-const getUpcomingTasks = () => {
-  const tasks = getAllTasks();
-  return tasks.filter((task) => isThisWeek(task.getDueDate()));
-};
-
-const getImportantTasks = () => {
-  const tasks = getAllTasks();
-  return tasks.filter((task) => task.getPriority() === "High");
-};
-
-const getCompletedTasks = () => {
-  const tasks = getAllTasks();
-  return tasks.filter((task) => task.getCompletedStatus() === true);
-};
-
-export {
-  getAllTasks,
-  getTodayTasks,
-  getUpcomingTasks,
-  getImportantTasks,
-  getCompletedTasks,
-};
+export { taskManager };
