95e7f72cd6617779583ebc4afa7241196d8a03bf
Amormio Velasquez III <amorbuks25@gmail.com>
2026-01-10 18:05:01 -0800

    Created event listener for adding tasks, working on a well-functioning dialog for creating the task

--- src/modules/calendarDOM.js
+++ src/modules/calendarDOM.js
@@ -1,61 +1,61 @@
-// import { getCalendarDates } from "./calendar";
+import { getCalendarDates } from "./calendar";
 
-// const monthYearElement = document.querySelector("#monthYear");
-// const prevBtn = document.querySelector("#prevBtn");
-// const nextBtn = document.querySelector("#nextBtn");
-// const datesElement = document.querySelector("#dates");
+const monthYearElement = document.querySelector("#monthYear");
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+const datesElement = document.querySelector("#dates");
 
-// export const renderCalendar = (year, month) => {
-//   // Clear previous dates
-//   while (datesElement.firstChild) {
-//     datesElement.removeChild(datesElement.firstChild);
-//   }
+export const renderCalendar = (year, month) => {
+  // Clear previous dates
+  while (datesElement.firstChild) {
+    datesElement.removeChild(datesElement.firstChild);
+  }
 
-//   const monthNames = [
-//     "January",
-//     "February",
-//     "March",
-//     "April",
-//     "May",
-//     "June",
-//     "July",
-//     "August",
-//     "September",
-//     "October",
-//     "November",
-//     "December",
-//   ];
-//   monthYearElement.textContent = `${monthNames[month]} ${year}`;
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  monthYearElement.textContent = `${monthNames[month]} ${year}`;
 
-//   const dates = getCalendarDates(year, month);
-//   dates.forEach(({ date, isCurrentMonth }) => {
-//     const dateElement = document.createElement("div");
-//     dateElement.classList.add("date");
-//     if (!isCurrentMonth) dateElement.classList.add("inactive");
-//     if (date.toDateString() === new Date().toDateString()) {
-//       dateElement.classList.add("active");
-//     }
-//     dateElement.textContent = date.getDate();
+  const dates = getCalendarDates(year, month);
+  dates.forEach(({ date, isCurrentMonth }) => {
+    const dateElement = document.createElement("div");
+    dateElement.classList.add("date");
+    if (!isCurrentMonth) dateElement.classList.add("inactive");
+    if (date.toDateString() === new Date().toDateString()) {
+      dateElement.classList.add("active");
+    }
+    dateElement.textContent = date.getDate();
 
-//     dateElement.addEventListener("click", () => {
-//       const activeDate = datesElement.querySelector(".date.active");
-//       if (activeDate) activeDate.classList.remove("active");
-//       dateElement.classList.add("active");
-//     });
+    dateElement.addEventListener("click", () => {
+      const activeDate = datesElement.querySelector(".date.active");
+      if (activeDate) activeDate.classList.remove("active");
+      dateElement.classList.add("active");
+    });
 
-//     datesElement.appendChild(dateElement);
-//   });
-// };
+    datesElement.appendChild(dateElement);
+  });
+};
 
-// prevBtn.addEventListener("click", () => {
-//   currentDate.setMonth(currentDate.getMonth() - 1);
-//   renderCalendar(currentDate.getFullYear(), currentDate.getMonth());
-// });
+prevBtn.addEventListener("click", () => {
+  currentDate.setMonth(currentDate.getMonth() - 1);
+  renderCalendar(currentDate.getFullYear(), currentDate.getMonth());
+});
 
-// nextBtn.addEventListener("click", () => {
-//   currentDate.setMonth(currentDate.getMonth() + 1);
-//   renderCalendar(currentDate.getFullYear(), currentDate.getMonth());
-// });
+nextBtn.addEventListener("click", () => {
+  currentDate.setMonth(currentDate.getMonth() + 1);
+  renderCalendar(currentDate.getFullYear(), currentDate.getMonth());
+});
 
-// const currentDate = new Date();
-// renderCalendar(currentDate.getFullYear(), currentDate.getMonth());
+const currentDate = new Date();
+renderCalendar(currentDate.getFullYear(), currentDate.getMonth());
--- src/modules/displayController.js
+++ src/modules/displayController.js
@@ -26,10 +26,32 @@ function setActivePage(event, sidebar) {
   loadPage();
 }
 
+function handleCreateTask() {
+  // plan
+  // maybe separate into multiple functions
+  // but this should be short and well organized such that when you open
+  // the calendar or dropdown list, clicking on anything after closes it.
+  // there probably shouldnt be so many if else blocks
+  const createTaskContainer = document.querySelector("dialog");
+  createTaskContainer.showModal();
+
+  createTaskContainer.addEventListener("click", (event) => {
+    if (event.target.matches(".select-date")) {
+      createTaskContainer.querySelector(".calendar").style.opacity = 1;
+    } else if (event.target.matches(".select-priority")) {
+      createTaskContainer.querySelector(".dropdown-list").style.opacity = 1;
+    }
+  });
+}
+
+function handleCreateProject() {}
 function displayWebsite() {
   const sidebar = document.querySelector(".sidebar");
   loadTodayTasks(); // load this by default
   sidebar.addEventListener("click", (event) => setActivePage(event, sidebar));
+
+  const addTaskButton = document.querySelector(".view-add-tasks");
+  addTaskButton.addEventListener("click", handleCreateTask);
 }
 
 export { displayWebsite };
